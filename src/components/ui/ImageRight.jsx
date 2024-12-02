2755384cb39d05a43746664dc036fbc42bbdcd36
MayONez98 <MayONez98@gmail.com>
2024-12-02 23:22:24 +0300

    line7

--- src/components/ui/ImageRight.jsx
+++ src/components/ui/ImageRight.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ImageRight() {
   return (
     <>
-      <img src="/img/imagee.png" alt="chartPhoto" className="chart" />
+      <img src="../../../img/imagee.png" alt="chartPhoto" className="chart" />
       <img src="/img/1.png" alt="photoAvaOne" className="ava ava-posOne" />
       <img src="/img/2.png" alt="photoAvaTwo" className="ava ava-posTwo" />
     </>
