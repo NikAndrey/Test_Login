dcd10360d917fd3229708d1eb886191d12813e57
MayONez98 <MayONez98@gmail.com>
2024-12-02 23:25:33 +0300

    line7

--- src/components/ui/ImageRight.jsx
+++ src/components/ui/ImageRight.jsx
@@ -4,7 +4,7 @@ export default function ImageRight() {
   return (
     <>
       <img src="../../img/imagee.png" alt="chartPhoto" className="chart" />
-      <img src="..//img/1.png" alt="photoAvaOne" className="ava ava-posOne" />
+      <img src="../img/1.png" alt="photoAvaOne" className="ava ava-posOne" />
       <img src="/img/2.png" alt="photoAvaTwo" className="ava ava-posTwo" />
     </>
   );
